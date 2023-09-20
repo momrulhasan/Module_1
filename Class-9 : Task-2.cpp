b6e222e501e47aa279d914f44e15d746e584ffb6
momrulhasan <88645857+momrulhasan@users.noreply.github.com>
2023-09-20 23:17:53 +0600

    Create Class-9 : Task-2.cpp

---
+++ Class-9 : Task-2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double n = 3.14159;
+
+int main() {
+    double R , A;
+    
+    cin>>R;
+    A = n*R*R;
+    cout << fixed << setprecision(4);
+    cout<<"A="<<A<<endl;
+ 
+    
+ 
+    return 0;
+}
