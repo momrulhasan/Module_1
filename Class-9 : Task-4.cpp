cdb4f8375d614cf1c88e16b19b4509970bb703e9
momrulhasan <88645857+momrulhasan@users.noreply.github.com>
2023-09-20 23:03:36 +0600

    Create Class-9 : Task-4.cpp

---
+++ Class-9 : Task-4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a ,b ;
+    cin >>a>>b;
+    int PROD = a*b;
+    cout <<"PROD = "<<PROD<<endl;
+ 
+    
+ 
+    return 0;
+}
