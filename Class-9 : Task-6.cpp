9938ab0096ddb1338573ad3b02bbe97c9e5b1429
momrulhasan <88645857+momrulhasan@users.noreply.github.com>
2023-09-22 12:19:38 +0600

    Create Class-9 : Task-6.cpp

---
+++ Class-9 : Task-6.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    float A,B,C,D;
+    cin>>A;
+    cin>>B;
+    cin>>C;
+    D=(A * 2 + B * 3+C*5) / (2+3+5);
+    cout<<"MEDIA = "<<fixed<<setprecision(1)<<D<<endl;
+ 
+    return 0;
+}
