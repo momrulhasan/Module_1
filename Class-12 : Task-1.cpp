fb6ebf6b1de90391162ef80c57fc5e33ac6ce41f
momrulhasan <88645857+momrulhasan@users.noreply.github.com>
2023-09-24 07:27:10 +0600

    Create Class-12 : Task-1.cpp

---
+++ Class-12 : Task-1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+    int m,N;
+    cin>>N;
+    if(5<N){
+        m= (N/6)+1;
+        cout<<m<<endl;
+    }else{
+        cout<<"1"<<endl;
+    }
+
+
+
+    return 0;
+}
