f805d9b0d1f1a36a47f82b4bd175a15332ce5e81
momrulhasan <88645857+momrulhasan@users.noreply.github.com>
2023-09-24 09:11:21 +0600

    Create Class-12 : Task-4.cpp

---
+++ Class-12 : Task-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p, q, y, r;
+    cin>>p;
+    cin>>q;
+    cin>>y;
+    r=y*52*(p*5+q*2);
+    cout<<r<<endl;
+
+
+    return 0;
+}
